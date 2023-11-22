bd29857ee00d64dbc53a4c194ebe076bc5fd4344
Arnau Saborit <arnausv@uoc.edu>
2023-11-22 17:31:39 +0100

    First version

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,17 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
+import * as bootstrap from 'bootstrap';
 
 /**
  * Write any other JavaScript below
  */
 
+/* Boilerplate default JS code
+
 +( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+*/
