044030ff8637e5e65c5ab072576bf53c7d784e6f
Arnau Saborit <arnausv@uoc.edu>
2023-11-29 10:00:34 +0100

    Web Test

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -19,3 +19,23 @@ import * as bootstrap from 'bootstrap';
 } )();
 
 */
+
+//Navbar link active class
+
+$(document).ready(function() {
+    // Obtén el nombre de la página actual
+    var currentPage = window.location.pathname.split('/').pop();
+
+    // Itera sobre los enlaces de la barra de navegación
+    $('.navbar-nav a').each(function() {
+        // Asegúrate de que el enlace tenga un atributo href antes de intentar acceder a él
+        if (this.hasAttribute('href')) {
+            var linkPage = this.href.split('/').pop();
+
+            // Compara la página actual con la página del enlace y agrega la clase "active" si son iguales
+            if (currentPage === linkPage) {
+                $(this).addClass('active');
+            }
+        }
+    });
+});
