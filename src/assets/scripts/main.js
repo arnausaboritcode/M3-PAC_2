e78fda46d08b6185959fdb8b802e19ce5aa58553
Arnau Saborit <arnausv@uoc.edu>
2023-12-02 11:53:24 +0100

    To revise

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,7 +5,7 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
-import * as bootstrap from 'bootstrap';
+
 
 /**
  * Write any other JavaScript below
@@ -23,19 +23,18 @@ import * as bootstrap from 'bootstrap';
 //Navbar link active class
 
 $(document).ready(function() {
-    // Obtén el nombre de la página actual
     var currentPage = window.location.pathname.split('/').pop();
-
-    // Itera sobre los enlaces de la barra de navegación
     $('.navbar-nav a').each(function() {
-        // Asegúrate de que el enlace tenga un atributo href antes de intentar acceder a él
         if (this.hasAttribute('href')) {
             var linkPage = this.href.split('/').pop();
-
-            // Compara la página actual con la página del enlace y agrega la clase "active" si son iguales
             if (currentPage === linkPage) {
                 $(this).addClass('active');
             }
         }
     });
 });
+
+//Initialize Aos library
+
+AOS.init();
+
